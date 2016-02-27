a174338310440778085a2a58bf7bc879aee47f8c
rvinicius <roberto396g@hotmail.com>
2016-02-27 14:44:39 -0300

    .

--- footer.php
+++ footer.php
@@ -1,10 +1,11 @@
-<footer class="row" style="padding:10px 15px 10px 15px; background-color:#6199fd;">
-		<div style="float:left; width: 120px;">
-			<span class="televendas">Televendas</span><br><span class="tele-numero">4002-8922</span>
+<footer class="col-md-12" style="margin:auto;padding:10px 15px 10px 15px; background-color:#6199fd;">
+		<div class="col-md-6" style="float:left; width: 120px;">
+			<p>Televendas</p>4002-8922
+			<!-- <img class="img-rounded" src="icons/follow.png">  -->
 		</div>
-		<img src="icons/follow.png">
-		<div style="margin:15px 0px 0px 450px;">
-			<p>Unicid - Rua Cesário Galeno 448/475 Tatuapé - São Paulo - SP CEP 03071-000</p>
-			<p>TimTimToys.com - Trabalho de Programação WEB - HTML, PHP, MySQL e Bootstrap</p>
+		<div class="col-md-6">
+			<p>Unicid - Rua Cesário Galeno 448/475 Tatuapé - São Paulo - SP CEP 03071-000 <br/>	
+			TimTimToys.com - Trabalho de Programação WEB - HTML, PHP, MySQL e Bootstrap</p>
 		</div>
+		<span class="span8"><img src="icons/follow.png" alt=""></span>
 </footer>
